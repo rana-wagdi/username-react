f634080eecfe98b1fdb0907debcf04a2b2804b5b
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-17 15:40:13 -0700

    add css

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Card.css'
 
 const Card = (props) => {
     return (
-        <div>
+        <div className='card'>{props.children}
         </div>
     )
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Card from '../UI/Card';
+
+import './AddUser.css'
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
     }
 
     return (
-      <Card>
-        <form onSubmit={addUserHandler}>
+      <Card >
+        <form className="input" onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
           <input id="username" type="text" />
           <label htmlFor="age">Age (Years)</label>
